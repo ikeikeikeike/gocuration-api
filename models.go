a188e7534443a1b856b65f9b258bc0d5065ff0fa
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-04-07 19:12:27 +0900

    tinyfix model

--- models.go
+++ models.go
@@ -9,57 +9,134 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type Image struct {
-	ID      int64
-	Name    string
-	Src     string
-	Ext     string
-	Mime    string
-	Width   int
-	Height  int
+type Entry struct {
+	Id          int64
+	Url         string
+	Title       string
+	Content     string
+	SeoTitle    string
+	SeoContent  string
+	Encoded     string
+	Creator     string
+	Publisher   string
+	PublishedAt time.Time
+
+	Q string
+
 	Created time.Time
 	Updated time.Time
+
+	Blog   *Blog
+	BlogId sql.NullInt64
+
+	// Video   *Video
+	Picture *Picture
+	// Summary *Summary
+
+	// Tags   []Tag   `gorm:"many2many:entry_tag;"`
+	// Images []Image `gorm:"many2many:entry_image;"`
+
+	// Scores []Score
+}
+
+type Blog struct {
+	Id          int64
+	Rss         string
+	Url         string
+	Name        string
+	Mediatype   string
+	Adsensetype string
+
+	VerifyLink  int
+	VerifyRss   int
+	VerifyParts int
+
+	IsPenalty bool
+
+	LastModified time.Time
+
+	Created time.Time
+	Updated time.Time
+
+	// User   User
+	// UserId sql.NullInt64
+
+	Icon   Image
+	IconId sql.NullInt64
+
+	// Scores  []Score
+	Entries []Entry
+}
+
+type Picture struct {
+	Id int64
+
+	Created time.Time
+	Updated time.Time
+
+	Entry   Entry
+	EntryId sql.NullInt64
+
+	Anime   Anime
+	AnimeId sql.NullInt64
+
+	Images     []Image
+	Characters []Character `gorm:"many2many:picture_character;"`
 }
 
 type Diva struct {
-	ID          int64
-	Name        string
-	Kana        string
-	Romaji      string
-	Gyou        string
-	Birthday    time.Time
-	Blood       string
-	Height      int
-	Weight      int
-	Bust        int
-	Waste       int
-	Hip         int
-	Bracup      string
-	Outline     string
-	Created     time.Time
-	Updated     time.Time
-	Icon        Image
-	IconID      sql.NullInt64
+	Id int64
+
+	Name   string
+	Kana   string
+	Romaji string
+	Gyou   string
+
+	Birthday time.Time
+
+	Blood string
+
+	Height int
+	Weight int
+
+	Bust   int
+	Waste  int
+	Hip    int
+	Bracup string
+
+	Outline string
+
+	Created time.Time
+	Updated time.Time
+
+	Icon   Image
+	IconId sql.NullInt64
+
 	VideosCount int
+	// Videos      []Video
 }
 
 type Anime struct {
-	ID            int64
-	Name          string
-	Alias         string
-	Kana          string
-	Romaji        string
-	Gyou          string
-	Url           string
-	Author        string
-	Works         string
-	ReleaseDate   time.Time
-	Outline       string
-	Created       time.Time
-	Updated       time.Time
-	Icon          Image
-	IconID        sql.NullInt64
-	Characters    []Character
+	Id          int64
+	Name        string
+	Alias       string
+	Kana        string
+	Romaji      string
+	Gyou        string
+	Url         string
+	Author      string
+	Works       string
+	ReleaseDate time.Time
+	Outline     string
+	Created     time.Time
+	Updated     time.Time
+
+	Icon   Image
+	IconId sql.NullInt64
+
+	Characters []Character
+
+	Pictures      []Picture
 	PicturesCount int
 }
 
@@ -81,11 +158,29 @@ type Character struct {
 	Created       time.Time
 	Updated       time.Time
 	Icon          Image
-	IconID        sql.NullInt64
+	IconId        sql.NullInt64
 	Product       string
 	Anime         Anime
-	AnimeID       sql.NullInt64
+	AnimeId       sql.NullInt64
 	PicturesCount int
+}
+
+type Image struct {
+	Id int64
+
+	Name string
+	Src  string
+
+	Ext    string
+	Mime   string
+	Width  int
+	Height int
+
+	Created time.Time
+	Updated time.Time
+
+	Picture   *Picture
+	PictureId sql.NullInt64
 }
 
 var DB gorm.DB
